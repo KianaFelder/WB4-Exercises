20de44c4c0a47d7cc25d06f056ed886accf731b9
KianaFelder <Kianafelder96@gmail.com>
2023-12-06 11:16:25 -0500

    Finding the answers

--- ArrayScripts/foods.js
+++ ArrayScripts/foods.js
@@ -1,15 +1,20 @@
 "use strict";
 
 let lunch = [
-    {item: "Steak Fajitas", price: 9.95},
+    {item: "Steak Fajitas", price: 16.95},
     {item: "Chips and Guacamole", price: 5.25},
     {item: "Sweet Tea", price: 2.79}
     ];
 
+    let total = 0
     let numOfItems = lunch.length
+    let totalTax = .08
+    let totalTip = .18
 
     for(let i = 0; i < numOfItems; i++){
-        //console.log(i);
-        console.log(lunch[i].price);
-        console.log(lunch[i].item)
+        total += lunch[i].price
     }
+    //console.log(i);
+    //console.log(lunch[i].price);
+
+    console.log("Your total is " + total)
